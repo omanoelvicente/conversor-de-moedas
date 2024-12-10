8a37e71fe2d754e7c216d665fda3423eeee6ef74
Manoel Vicente <manoelj.vicente@gmail.com>
2024-12-10 13:14:08 +0000

    feat: Create Domain class Moeda

--- src/Moeda.java
+++ src/Moeda.java
@@ -1,2 +1,57 @@
+import java.math.BigDecimal;
+import java.math.RoundingMode;
+import java.time.LocalDateTime;
+
 public class Moeda {
+
+    private final String codigoMoedaBase;
+    private final String codigoMoedaAlvo;
+    private BigDecimal valorAConverter;
+    private final BigDecimal taxaDeConversao;
+    private final BigDecimal valorConvertido;
+    private LocalDateTime dataDeCriacao;
+
+
+    public Moeda (MoedaDTO moedaDTO ) {
+        this.dataDeCriacao   = dataDeCriacao;
+        this.codigoMoedaBase = moedaDTO.base_code();
+        this.codigoMoedaAlvo = moedaDTO.target_code();
+        this.valorAConverter = valorAConverter;
+        this.taxaDeConversao = moedaDTO.conversion_rate().setScale(2, RoundingMode.HALF_EVEN);
+        this.valorConvertido = moedaDTO.conversion_result().setScale(2, RoundingMode.HALF_EVEN);
+
+    }
+
+    public LocalDateTime getDataDeCriacao() {
+        return dataDeCriacao;
+    }
+
+    public void setDataDeCriacao(LocalDateTime dataDeCriacao) {
+        this.dataDeCriacao = dataDeCriacao;
+    }
+
+    public String getCodigoMoedaBase() {
+        return codigoMoedaBase;
+    }
+
+    public String getCodigoMoedaAlvo() {
+        return codigoMoedaAlvo;
+    }
+
+    public BigDecimal getTaxaDeConversao() {
+        return taxaDeConversao;
+    }
+
+    public BigDecimal getValorConvertido() {
+        return valorConvertido;
+    }
+
+    public BigDecimal getValorAConverter() {
+        return valorAConverter;
+    }
+
+    public void setValorAConverter(BigDecimal valorAConverter) {
+        this.valorAConverter = valorAConverter;
+    }
+
 }
