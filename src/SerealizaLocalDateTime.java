a8f4b4d5fd74478b76343036c58a60611a2cada0
Manoel Vicente <manoelj.vicente@gmail.com>
2024-12-10 18:50:43 +0000

    feat: Add SerealizaDateTime class

--- src/SerealizaLocalDateTime.java
+++ src/SerealizaLocalDateTime.java
@@ -1,2 +1,18 @@
-public class SerealizaLocalDateTime {
+import com.google.gson.JsonElement;
+import com.google.gson.JsonPrimitive;
+import com.google.gson.JsonSerializationContext;
+import com.google.gson.JsonSerializer;
+import java.lang.reflect.Type;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+// Classe para serializar LocalDataTime
+public class SerealizaLocalDateTime implements JsonSerializer<LocalDateTime> {
+    //Formata a data e hora
+    private static final DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd-MMM-yyyy HH:mm:ss");
+
+    @Override
+    public JsonElement serialize(LocalDateTime localDateTime, Type type, JsonSerializationContext jsonSerializationContext) {
+        return new JsonPrimitive(formatter.format(localDateTime));
+    }
+
 }
